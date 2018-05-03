05438248145a63c952a148795e0003172afe3e63
Stuart Colville <scolville@mozilla.com>
2018-05-03 10:46:31 +0100

    Fix stream handling

--- src/background.js
+++ src/background.js
@@ -2,10 +2,19 @@ function listener(details) {
   let filter = browser.webRequest.filterResponseData(details.requestId);
   let decoder = new TextDecoder("utf-8");
   let encoder = new TextEncoder();
+  let response = [];
+
+  filter.onstart = event => {
+    response = [];
+  }
 
   filter.ondata = event => {
-    let str = decoder.decode(event.data, {stream: false});
-    const jsonResponse = JSON.parse(str);
+    let str = decoder.decode(event.data, {stream: true});
+    response.push(str);
+  }
+
+  filter.onstop = event => {
+    const jsonResponse = JSON.parse(response.join(''));
     jsonResponse.authors = [];
     filter.write(encoder.encode(JSON.stringify(jsonResponse)));
     filter.disconnect();
@@ -18,7 +27,7 @@ browser.webRequest.onBeforeRequest.addListener(
   listener,
   { urls: [
     "https://addons-dev.allizom.org/api/v3/addons/addon/*",
-    "https://addons.allizom.org/api/v3/addons/addon/*"
+    "https://addons.allizom.org/api/v3/addons/addon/*",
   ], types: ["main_frame"]},
   ["blocking"]
 );
